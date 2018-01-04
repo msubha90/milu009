2b5ccc589cbe0ce3ecf9d95652c3c164ae096dfb
msubha90 <msubhajitsahoo@gmail.com>
2018-01-05 00:29:32 +0530

    modified
    Signed-off-by: msubha90 <msubhajitsahoo@gmail.com>

--- Demo/src/Git/Test01.java
+++ Demo/src/Git/Test01.java
@@ -5,7 +5,7 @@ public class Test01 {
 	public static void main(String[] args) 
 	{
 		
-		System.out.println("Git repository demo and modified.");
+		System.out.println("Git repository demo and modified 000.");
 	
 	}
 
